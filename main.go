95bb415993f1467dd3fb944fecde58d87ab3a59e
Abinesh09 <149653551+Abinesh09@users.noreply.github.com>
2025-01-29 11:04:29 +0530

    Create's MVC in go

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+    "log"
+    "net/http"
 )
 
 func main() {
-	fmt.Println("Hello Go_Web!")
+
+    log.Println("Server started at http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+
